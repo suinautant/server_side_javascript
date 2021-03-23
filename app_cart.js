4c569ec96e75246ef3b1b0fb9c70bb2f0442300d
suinautant <suinautant@gmail.com>
2021-03-23 22:32:16 +0900

    app cookies - shopping cart

---
+++ app_cart.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+app.set('views', './views_cart');
+app.locals.pretty = true;
+
+app.get('/cart', (req, res) => {
+    var num = req.query.num;
+    var action = req.query.action;
+
+    res.send('num : ' + num + ', action : ' + action);
+
+    // if(action = 'add'){
+    //     // add list in cart
+    //     var cart = [
+    //         { num, ${num} },
+    //         { action, ${action}}
+    //     ];
+    // } else if (action = 'del') {
+    //     // dele list in cart
+    // }
+    // res.render('cart');
+});
+
+app.get('/product', (req, res) => {
+    res.render('product');
+});
+
+app.listen(3003);
--- app_cookie.js
+++ app_cookie.js
@@ -3,6 +3,72 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 
+// 삭제 기능 추가 할 것
+
+var products = {
+    1: { title: 'The history of web' },
+    2: { title: 'The next web' },
+};
+
+app.get('/products', (req, res) => {
+    var output = '';
+    for (var name in products) {
+        output += `
+        <li>
+            <a href="/cart/${name}">${products[name].title}</a>
+        </li>
+        `;
+    }
+    output = `
+    <h1>Products</h1>
+    <ul>
+        ${output}
+    </ul>
+    <h2><a href="/cart">Cart</a></h2>
+    `;
+    res.send(output);
+});
+
+app.get('/cart/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (req.cookies.cart) {
+        var cart = req.cookies.cart;
+    } else {
+        var cart = {};
+    }
+
+    if (!cart[id]) {
+        cart[id] = 0;
+    }
+    cart[id] = parseInt(cart[id]) + 1;
+    res.cookie('cart', cart);
+    res.redirect('/cart');
+});
+
+app.get('/cart', (req, res) => {
+    var output = '';
+    if (req.cookies.cart) {
+        var cart = req.cookies.cart;
+
+        for (var name in products) {
+            if (cart[name]) {
+                output += `
+                    <li>${products[name].title} : ${cart[name]}</li>
+                `;
+            }
+        }
+    } else {
+        res.send('Empty');
+    }
+    output = `
+    <h1>Cart</h1>
+    ${output}
+    <h2><a href="/products">Products</a></h2>
+    `;
+    res.send(output);
+});
+
 app.get('/count', (req, res) => {
     if (req.cookies.count) {
         var count = parseInt(req.cookies.count);
