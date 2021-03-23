accf341373a93909c6412658b08cfa2eb67884ae
suinautant <suinautant@gmail.com>
2021-03-23 16:47:23 +0900

    cookie count

--- app_cookie.js
+++ app_cookie.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
+
+app.get('/count', (req, res) => {
+    if (req.cookies.count) {
+        var count = parseInt(req.cookies.count);
+    } else {
+        var count = 0;
+    }
+    count = count + 1;
+
+    res.cookie('count', count);
+    res.send('count : ' + count);
+});
 
 app.listen(3003, () => {
     console.log('connected http://localhost:3003 !!');
